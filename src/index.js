7af2167c6880ea9573cc71c913d0f0676fbc2eb9
BEST3049 <dharmeesh3049@gmail.com>
2024-02-09 11:14:25 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './Home/Homepage';
+import Home from './Components/Home';
+import Navbar from './Components/navbar';
+import Contact from './Components/contact';
+import Therapist from './Components/Therapist';
+import Login from './Components/Login';
+import Music from './Components/Music';
+import Guide from './Components/Guide';
+import Cathybrown from './Components/cathybrown';
+import Pratik from './Components/Pratik';
+function MainApp()
+{
+  return(
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/homepage" element={<Homepage/>}/> */}
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/navbar" element={<Navbar/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/therapist" element={<Therapist/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/music" element={<Music/>}/>
+        <Route path="/cathy_brown" element={<Cathybrown/>}/>
+        <Route path="/guide" element={<Guide/>}/>
+        <Route path="/pratik" element={<Pratik/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+ReactDOM.render(<MainApp />, document.getElementById('root'));
